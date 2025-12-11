25d7c752bb32aec1758ddfb4d7ea9ba64c50b111
Oumar2025 <hp.oumaroulife2023@gmail.com>
2025-12-12 00:35:59 +0700

    Add clean-app: Express MVC with Prisma and SQLite - Following PDF instructions

---
+++ clean-app/services/userService.js
@@ -0,0 +1,17 @@
+const prisma = require('../prisma/client');
+
+const getAllUsers = async () => {
+  const users = await prisma.user.findMany();
+  return users;
+}
+
+const createUser = async (user) => {
+  const newUser = await prisma.user.create({ data: user });
+  return newUser;
+}
+
+module.exports = {
+  getAllUsers,
+  createUser
+}
+
